072a1a307a93f241604b82c44fa394f495d2bc0e
Sarai Hernandez Munives <prinxezita.hdz@gmail.com>
2013-03-20 08:00:28 -0600

    version 1.0

---
+++ js/Acciones/acciones.js
@@ -0,0 +1,17 @@
+// JavaScript Document
+$(document).ready(function(e)
+
+$('#izquierda´). swipe left (function (){
+	navigator.notification.confirm ('¿QUE DESEA HACER?; function (opcion){
+		switch (opcion){
+			case 1:
+			navigator.notification.beep (1);
+			break;
+			case 2:
+			navigator.notification.vibrate (500);
+			break;
+		}//switch
+	}"practica 1" "beep, vibrar, cancelar"); //function confirm
+});//function swipe
+{
+});
